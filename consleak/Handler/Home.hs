4914bc63e093901e8b716e9cc061e48efb84a8ae
Felipe Lessa <felipe.lessa@gmail.com>
2013-06-05 14:26:39 -0300

    Form for adding questions.

--- consleak/Handler/Home.hs
+++ consleak/Handler/Home.hs
@@ -3,37 +3,31 @@ module Handler.Home where
 
 import Import
 
--- This is a handler function for the GET request method on the HomeR
--- resource pattern. All of your resource patterns are defined in
--- config/routes
---
--- The majority of the code you will write in Yesod lives in these handler
--- functions. You can spread them across multiple files if you are so
--- inclined, or create a single monolithic file.
+
+-- | Homepage for the consleak site.
 getHomeR :: Handler RepHtml
 getHomeR = do
-    (formWidget, formEnctype) <- generateFormPost sampleForm
-    let submission = Nothing :: Maybe (FileInfo, Text)
-        handlerName = "getHomeR" :: Text
-    defaultLayout $ do
-        aDomId <- newIdent
-        setTitle "Welcome To Yesod!"
-        $(widgetFile "homepage")
+  (formWidget, formEnctype) <- generateFormPost questionForm
+  defaultLayout $ do
+    setTitleI MsgWelcome
+    $(widgetFile "homepage")
+
 
 postHomeR :: Handler RepHtml
 postHomeR = do
-    ((result, formWidget), formEnctype) <- runFormPost sampleForm
-    let handlerName = "postHomeR" :: Text
-        submission = case result of
-            FormSuccess res -> Just res
-            _ -> Nothing
+  ((result, _formWidget), _formEnctype) <- runFormPost questionForm
+  case result of
+    FormSuccess question -> do
+      _questionId <- runDB $ insert question
+      setMessageI (MsgQuestionPosted question)
+      redirect HomeR
+    _ -> do
+      setMessage "Error with question form"
+      redirect HomeR
 
-    defaultLayout $ do
-        aDomId <- newIdent
-        setTitle "Welcome To Yesod!"
-        $(widgetFile "homepage")
 
-sampleForm :: Form (FileInfo, Text)
-sampleForm = renderDivs $ (,)
-    <$> fileAFormReq "Choose a file"
-    <*> areq textField "What's on the file?" Nothing
+questionForm :: Form Question
+questionForm = renderDivs $
+  Question
+    <$>                 areq textField     (fieldSettingsLabel MsgQuestionTitle)       Nothing
+    <*> (unTextarea <$> areq textareaField (fieldSettingsLabel MsgQuestionDescription) Nothing)
